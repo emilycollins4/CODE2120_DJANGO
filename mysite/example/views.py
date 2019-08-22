d13a33a3e0d0778674be206674afef5ae9e72c61
Emily Collins <emtcollins4@gmail.com>
2019-08-22 17:34:06 +1000

    new repo

---
+++ mysite/example/views.py
@@ -0,0 +1,72 @@
+
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+import json
+from .models import *
+import os, sys
+
+# Create your views here.
+
+def example_get(request, var_a, var_b):
+	try:
+		returnob = {
+		"data": "%s is really silly x %s" %(var_a, var_b),
+		}
+		return JsonResponse(returnob)
+	except Exception as e:
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		other = sys.exc_info()[0].__name__
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		errorType = str(exc_type)
+		return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+
+@csrf_exempt
+def fib(request):
+	jsob = {"startNumber": 0, "length": 10}
+	log = []
+	if request.method == "POST":
+		try:
+			data = request.POST["data"]
+			received = json.loads(data)
+			jsob.update(received)
+			####required^
+			startNumber = int(jsob["startNumber"])
+			length = int(jsob["length"])
+			loop = range(length)
+
+			numarray = []
+			fibno = startNumber
+			addno = 1
+			for l in loop:
+				numarray.append(fibno)
+				fibno = fibno +addno
+				addno = fibno - addno
+
+
+
+			return JsonResponse({"fib":numarray})
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+	else:
+		return JsonResponse(jsob)
+
+@csrf_exempt
+def example_post(request):
+	log = []
+	if request.method == "POST":
+		try:
+			data = request.POST["data"]
+			return JsonResponse({"log":log})
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+	else:
+		return JsonResponse(jsob)
